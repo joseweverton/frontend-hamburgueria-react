95d939b8e8b852fc0893e01ca9b47ec8606ed0e6
jwcosta-br <jwcosta.br@gmail.com>
2024-01-23 10:08:46 -0300

    Enviando peidido para a API

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
 
+import { useCart } from '../../hooks/CartContext'
+import api from '../../services/api'
+import formatCurrency from '../../utils/formatCurrency'
 import { Button } from '../Button'
 import { Container } from './styles'
 
 export function CartResume() {
+  const [finalPrice, setFinalPrice] = useState(0)
+  const [deliveryTax] = useState(5)
+
+  const { cartProducts } = useCart()
+
+  useEffect(() => {
+    const sumAllItems = cartProducts.reduce((acc, current) => {
+      return current.price * current.quantity + acc
+    }, 0)
+    setFinalPrice(sumAllItems)
+  }, [cartProducts, deliveryTax])
+
+  const submitOrder = async () => {
+    const order = cartProducts.map(product => {
+      return { id: product.id, quantity: product.quantity }
+    })
+
+    await toast.promise(api.post('orders', { products: order }), {
+      pending: 'Processando seu pedido',
+      success: 'Pedido realizado com suscesso',
+      error: 'Falha ao tentar realizar o seu pedido, tente novamente'
+    })
+  }
+
   return (
     <div>
       <Container>
         <div className="container-top">
           <h2 className="title">Resumo do pedido</h2>
           <p className="items">Itens</p>
-          <p className="items-price">R$ 10,00</p>
+          <p className="items-price">{formatCurrency(finalPrice)}</p>
           <p className="delivery-tax">Taxa de entrega</p>
-          <p className="delivery-tax-price">R$ 10,00</p>
+          <p className="delivery-tax-price">{formatCurrency(deliveryTax)}</p>
         </div>
         <div className="container-bottom">
           <p>Total</p>
-          <p>R$ 20,00</p>
+          <p>{formatCurrency(finalPrice + deliveryTax)}</p>
         </div>
       </Container>
-      <Button style={{ width: '100%', marginTop: 30 }}>Finalizar Pedido</Button>
+      <Button style={{ width: '100%', marginTop: 30 }} onClick={submitOrder}>
+        Finalizar Pedido
+      </Button>
     </div>
   )
 }
